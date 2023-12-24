717aebf2bc9647416eb5d9f12ff523f98263b7ae
Farid <faridforward222@gmail.com>
2023-12-24 23:24:00 +0330

    added https for next config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    NEXT_PUBLIC_API_URL: "http://amir-gold.runflare.run/api/v1",
-    NEXT_PUBLIC_API_BASE_URL: "http://amir-gold.runflare.run",
+    NEXT_PUBLIC_API_URL: "https://amir-gold.runflare.run/api/v1",
+    NEXT_PUBLIC_API_BASE_URL: "https://amir-gold.runflare.run",
     NEXT_PUBLIC_BASE_URL: "https://frontend.runflare.run",
   },
 };
